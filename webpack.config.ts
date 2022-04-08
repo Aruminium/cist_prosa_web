df1b8dc96d62b278cdc4936e6f12cdf509e7ca6f
Aruminium <b2201680@photon.chitose.ac.jp>
2022-04-08 14:33:21 +0900

    changed setting

---
+++ webpack.config.ts
@@ -0,0 +1,12 @@
+// import * as path from 'path';
+// import { Configuration } from 'webpack';
+
+// const config: Configuration = {
+// 	context: path.join(__dirname, 'src'),
+// 	entry: './index.tsx',
+// 	output: {
+// 		path: path.join(__dirname, 'dist'),
+// 		filename: 'bundle.js',
+// 		publicPath: '/'
+// 	}
+// }
